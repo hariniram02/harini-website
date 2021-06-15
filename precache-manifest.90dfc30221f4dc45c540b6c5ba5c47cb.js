0df653959d6baf6f29120e1271521bb0fe9d4b5d
Harini Ram <hariniram@Harinis-MacBook-Air.local>
2021-06-15 15:23:10 -0500

    Updates

---
+++ precache-manifest.90dfc30221f4dc45c540b6c5ba5c47cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d26cd002e1ee052f0c7dd4524c17019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5bb6e61d67ecaf577d0",
+    "url": "/static/css/2.ac0fbea9.chunk.css"
+  },
+  {
+    "revision": "cf7c502f0f94ebf997c9",
+    "url": "/static/css/main.4e79c0f6.chunk.css"
+  },
+  {
+    "revision": "f5bb6e61d67ecaf577d0",
+    "url": "/static/js/2.be6aa95d.chunk.js"
+  },
+  {
+    "revision": "70b070bc4c4b879d3d3b9683ea8bec9f",
+    "url": "/static/js/2.be6aa95d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf7c502f0f94ebf997c9",
+    "url": "/static/js/main.83eb6537.chunk.js"
+  },
+  {
+    "revision": "27c4c1e8bd273030f1c3",
+    "url": "/static/js/runtime-main.54d53259.js"
+  },
+  {
+    "revision": "c112eac3873a3e8dd3ae2874566a6093",
+    "url": "/static/media/me.c112eac3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/harini-website/precache-manifest.39fc23a1a4710af96c9eb0175fceb3e4.js"
+  "/precache-manifest.90dfc30221f4dc45c540b6c5ba5c47cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/harini-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
